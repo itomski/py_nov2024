65df8eba9a40fa57c0bd6190fbf2586182e16d28
Tomasz Lubowiecki <tomasz@lubowiecki.de>
2024-11-29 13:19:10 +0100

    Aufgabe 3

---
+++ uebung3.py
@@ -0,0 +1,19 @@
+#Aufgabe 3
+# Entwerfe eine Sammlung (Modul) an Funktionen für einen Würfelbecher
+# Funktionen: 
+# Ein Wurf mit einem sechsseitigen Würfel. 
+# Das Ergebnis soll als int zurückgegeben werden.
+
+# Ein Wurf mit mehreren sechsseitigen Würfeln. 
+# Das Ergebnis soll als eine Int-Liste zurückgegeben werden. 
+# Bei Aufruf dieser Funktion wird die gewünschte Anzahl der Würfel als Int-Argument übergeben.
+
+# Auswertung eines Wurfs mit mehreren Würfeln. 
+# Hierbei wird das Ergebnis der zweiten Funktion als Argument übergeben. 
+# Es folgt eine Konsolen-Ausgabe nach folgendem Schema:
+# Wurf-Ergebnis: 
+# x mal 1: 
+# x mal 2: 
+# x mal 3: 
+# x mal 
+# etc.
