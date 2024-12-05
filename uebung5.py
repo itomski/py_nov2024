1942ca6c1fd0d911f389d305c0ca4d39261cee54
Tomasz Lubowiecki <tomasz@lubowiecki.de>
2024-12-05 08:27:00 +0100

    Übungen

--- uebung5.py
+++ uebung5.py
@@ -6,3 +6,34 @@
 # Über einen Konstruktor sollen die Attribute initialisiert werden.
 #
 # Erstellen Sie in der Main einen Song und rufen Sie die Methode auf.
+
+class Song:
+    def __init__(self, titel, dauer_sekunden, interpret): # Konstruktor
+        self.__titel = titel
+        self.__dauer_sekunden = dauer_sekunden
+        self.__interpret = interpret
+
+    def spielen(self):
+        titel = self.__titel
+        dauer_minuten = int(self.__dauer_sekunden / 60) # Nachkommastelle entfernen
+        dauer_sekunden_rest = self.__dauer_sekunden % 60
+        interpret = self.__interpret
+        #return "Titel: " + titel \
+        #       + " Dauer: " + str(dauer_minuten).zfill(2) + ":" + str(dauer_sekunden_rest).zfill(2) \
+        #       + " Interpret: " + interpret
+
+        return f"Titel: {titel} \nDauer: {dauer_minuten:02d}:{dauer_sekunden_rest:02d} \nInterpret: {interpret}"
+
+
+    def get_titel(self):
+        return self.__titel
+
+    def get_dauer_sekunden(self):
+        return self.__dauer_sekunden
+
+    def get_interpret(self):
+        return self.__interpret
+
+
+song1 = Song("Neon", 259, "Amplifire")
+print(song1.spielen())
--- uebung6.py
+++ uebung6.py
@@ -6,3 +6,64 @@
 # Dazu müssen Sie den Klassen weitere Member hinzufügen.
 # (Gehen Sie davon aus, dass ein Buch mehrere Autoren haben und ein Autor mehrere Bücher verfassen kann)
 # Testen Sie das Programm im Main.
+
+class Book:
+    def __init__(self, title, authors):
+        self.__title = title
+        self.__authors = authors
+
+        a: Author  # Typ-Hinweis
+        for a in authors:
+            a.get_books().append(self)
+
+    def get_title(self):
+        return self.__title
+
+    def set_authors(self, authors):
+        self.__authors = authors
+
+    def get_authors(self):
+        return self.__authors
+
+    def __str__(self):
+        string = "Title: " + self.__title + "\n" + "Authors: \n"
+        for a in self.__authors:
+            string += str(a) + "\n"
+        return string
+
+
+class Author:
+    def __init__(self, name):
+        self.__name = name
+        self.__books = []
+
+    def get_name(self):
+        return self.__name
+
+    def set_books(self, books):
+        self.__books = books
+
+    def get_books(self):
+        return self.__books
+
+    def __str__(self):
+        return "Name: " + self.__name
+
+
+a1 = Author("William Gibson")
+a2 = Author("Bruce Sterling")
+a3 = Author("Stanislaw Lem")
+
+b1 = Book("Idoru", [a1])
+b2 = Book("Solaris", [a3])
+b3 = Book("Die Differenzmaschine", [a1, a2])
+
+# Ausgabe aller Books der jeweiligen Authors
+for b in a1.get_books():
+    print(b)
+
+for b in a2.get_books():
+    print(b)
+
+for b in a3.get_books():
+    print(b)
